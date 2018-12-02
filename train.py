9d2f51c45ee21ffdab6f66913e86a6a15d502881
zsimone10 <zensimone10@gmail.com>
2018-12-01 17:16:48 -0800

    DT edits

--- train.py
+++ train.py
@@ -36,9 +36,9 @@ def load_data(path):
     #data = data.drop([0], axis=0)
     return data
     #RETURN data.as_matrix()
-x = load_data('most_seasons_unnormalized.csv')
+x = load_data('data/most_seasons_unnormalized.csv')
 #print(x)
-y = load_data('labels_most_seasons.csv')
+y = load_data('data/labels_most_seasons.csv')
 
 ############
 #RESAMPLE
